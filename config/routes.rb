ded1a9417c813b16574c58cf06bb7aab1be0b38f
RinatGibadullin <Rinat990104@yandex.ru>
2020-01-27 20:37:01 +0300

    added slim and fixed views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'users/profile'
-	root to: "events#index"
-	resources :events
+  devise_for :users, :controllers => { registrations: 'registrations'}
+  get '/profile', to: 'users#profile'
+  root to: "events#index"
+  resources :events
+
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
